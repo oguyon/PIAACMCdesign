1c2949d0d0ece4ef0095a4dcd6522b98b628fa83
oguyon <oliv.guyon@gmail.com>
2015-09-10 14:34:36 -1000

    improved bash scripts: more options

--- search/variables_c.js
+++ search/variables_c.js
@@ -39,6 +39,7 @@ var searchData=
   ['piaaconfr0fr1',['piaaconfr0fr1',['../coronagraphs_8c.html#acff6f4c13f4b75acfbbe4257da8025df',1,'coronagraphs.c']]],
   ['piaaconfr1',['piaaconfr1',['../coronagraphs_8c.html#a96b26d7d83d035809e6081efe8efad4b',1,'coronagraphs.c']]],
   ['piaaconfr1fr0',['piaaconfr1fr0',['../coronagraphs_8c.html#ae619e7cfd485bd20ec1b0b063ea66086',1,'coronagraphs.c']]],
+  ['piaacpamax',['piaaCPAmax',['../structOPTPIAACMCDESIGN.html#a3fa5fbe424c220ac11e527ae5081e2c7',1,'OPTPIAACMCDESIGN']]],
   ['piaaextfactor0',['PIAAextfactor0',['../coronagraphs_8c.html#a95c57ae15c1198f86a900eb96a2f6b0c',1,'coronagraphs.c']]],
   ['piaaextfactor1',['PIAAextfactor1',['../coronagraphs_8c.html#ae9921e52709b29a068f82f7c224d817a',1,'coronagraphs.c']]],
   ['piaafluxfactor',['PIAAFLUXFACTOR',['../coronagraphs_8c.html#a029bada1f77ca24464c7c0bd6e0602d9',1,'coronagraphs.c']]],
@@ -47,6 +48,7 @@ var searchData=
   ['piaalowfs',['PIAALOWFS',['../coronagraphs_8c.html#a8fea40b70a6dd939a745b785966a9683',1,'coronagraphs.c']]],
   ['piaamaterial_5fcode',['PIAAmaterial_code',['../structOPTPIAACMCDESIGN.html#a9fc6f0b8de4e0e7fe0694cd8f4e827e1',1,'OPTPIAACMCDESIGN']]],
   ['piaamaterial_5fname',['PIAAmaterial_name',['../structOPTPIAACMCDESIGN.html#abf3280f1ad15a23b5cf9f05942ca6d69',1,'OPTPIAACMCDESIGN']]],
+  ['piaanbcmodesmax',['piaaNBCmodesmax',['../structOPTPIAACMCDESIGN.html#a8fbed9f02c0a17dcc2f233be4c4af697',1,'OPTPIAACMCDESIGN']]],
   ['piaaoversize',['PIAAOVERSIZE',['../coronagraphs_8c.html#a88eee1afed8e2e27e720df4cbd437b0b',1,'coronagraphs.c']]],
   ['piaasep',['piaasep',['../structOPTPIAACMCDESIGN.html#ab9b00e2fc69b7a0b11354d5cdf718635',1,'OPTPIAACMCDESIGN']]],
   ['pitch',['pitch',['../structDM__SIM.html#ade1c585e1ec4dff6446ba9e8349082d0',1,'DM_SIM']]],
@@ -54,6 +56,10 @@ var searchData=
   ['polycoeff1',['polycoeff1',['../linopt__imtools_8c.html#a3cbf724daa6ced25bc4ddf7d17a05f0a',1,'linopt_imtools.c']]],
   ['polycoeff2',['polycoeff2',['../linopt__imtools_8c.html#a05e6847679b5d1fedf8b5f13bd37417c',1,'linopt_imtools.c']]],
   ['pos',['pos',['../structkdnode.html#af92e7d95a2845d3e8611b289d6398a83',1,'kdnode']]],
+  ['postpiaa0mask',['postpiaa0mask',['../structOPTPIAACMCDESIGN.html#a392b32b63ba511bff062e98c941814aa',1,'OPTPIAACMCDESIGN']]],
+  ['postpiaa0maskpos',['postpiaa0maskpos',['../structOPTPIAACMCDESIGN.html#a5fb317d6c29f6d774462ae4f6b6c3e09',1,'OPTPIAACMCDESIGN']]],
   ['precision',['precision',['../structDATA.html#a7b6fce0fc9e6046ffa57b30b8bcd0d1f',1,'DATA']]],
+  ['prepiaa0mask',['prepiaa0mask',['../structOPTPIAACMCDESIGN.html#a4f424ef8e93fc3f0a89c37a9c5972820',1,'OPTPIAACMCDESIGN']]],
+  ['prepiaa0maskpos',['prepiaa0maskpos',['../structOPTPIAACMCDESIGN.html#a4f6c7b87f374eefda6c212329be7ba43',1,'OPTPIAACMCDESIGN']]],
   ['processname',['processname',['../structDATA.html#a58978653cfb98eaf9b9007cb992cc0e4',1,'DATA']]]
 ];
